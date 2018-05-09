02b0b776c61bfe44ace85a302ab070e289c3581f
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-05-09 18:26:27 +0200

    Napravljena je klasa Artikal

--- QuickServe/src/artikal/Artikal.java
+++ QuickServe/src/artikal/Artikal.java
@@ -1,5 +1,36 @@
 package artikal;
 
+import java.util.LinkedList;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 public class Artikal {
 
+	public String naziv;
+
+	public double cena;
+
+	public LinkedList<String> sastojci;
+
+	public String slika;
+
+	public Icon image;
+
+	public String tip;
+
+	public Artikal(double cena, String naziv, String putanjaSlike, LinkedList<String> sastojci, String tip) {
+		this.naziv = naziv;
+		this.cena = cena;
+		image = new ImageIcon(getClass().getResource(putanjaSlike));
+		this.sastojci = sastojci;
+		this.tip = tip;
+	}
+
+	public Artikal(double cena, String naziv, String tip) {
+		this.naziv = naziv;
+		this.cena = cena;
+		this.tip = tip;
+
+	}
 }
